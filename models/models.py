59d00e9bbb8d1520a29429ac680aded39f6189c2
smartboxit <smartbox.it@gmail.com>
2019-11-24 17:30:38 +0700

    4th update from laptop

--- models/models.py
+++ models/models.py
@@ -11,4 +11,23 @@ class CarRequest(models.Model):
     name=fields.char(string="Request",required="true")
     date_from=fields.datetime(string="Starting Date",default=fields.datetime.now)
     date_to=fields.datetime(string="End Date",required="false")
-
+    employee_id = fields.Many2one(
+        string='employee',
+        comodel_name='hr.employee',
+        ondelete='restrict',
+        required=True
+    )   
+    car_id = fields.Many2one(
+        string='car',
+        comodel_name='fleet.vehicle',
+        ondelete='restrict', 
+        required=True
+    ) 
+    state = fields.Selection(
+        string='Status',
+        selection=[('draft','Draft') , ('confirm','Confirm') , ('validate','Validate') , ('refuse','Refuse') , ('approve','Approve') ],
+        default='draft'
+    )
+    
+                                
+    
