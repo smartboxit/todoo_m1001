20e1824f930fc7aacc890e143f6c9d1617025e93
smartboxit <smartbox.it@gmail.com>
2019-11-25 16:08:14 +0700

    switch to dell

--- models/models.py
+++ models/models.py
@@ -8,22 +8,22 @@ class CarRequest(models.Model):
     _name="car.request"   #db table name = card.request
     _description="car request"
 
-    name=fields.char(string="Request",required="true")
-    date_from=fields.datetime(string="Starting Date",default=fields.datetime.now)
-    date_to=fields.datetime(string="End Date",required="false")
-    employee_id = fields.Many2one(
+    name=fields.Char(string="Request",required=True)
+    date_from=fields.Datetime(string="Starting Date",default=fields.datetime.now())
+    date_to=fields.Datetime(string="End Date",required=False)
+    employee_id=fields.Many2one(
         string='employee',
         comodel_name='hr.employee',
         ondelete='restrict',
         required=True
     )   
-    car_id = fields.Many2one(
+    car_id=fields.Many2one(
         string='car',
         comodel_name='fleet.vehicle',
         ondelete='restrict', 
         required=True
     ) 
-    state = fields.Selection(
+    state=fields.Selection(
         string='Status',
         selection=[('draft','Draft') , ('confirm','Confirm') , ('validate','Validate') , ('refuse','Refuse') , ('approve','Approve') ],
         default='draft'
